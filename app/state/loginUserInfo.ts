333ae72deaf5a90f7b11a44d2fe0369f9c52b172
Kazuki Takane <highjumptakane@gmail.com>
2023-07-30 11:12:40 +0900

    fetch機能追加、ログインユーザー詳細・リポジトリ一覧表示

---
+++ app/state/loginUserInfo.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const loginUserInfo = atom<any>({
+  key: "loginUserInfo",
+  default: {},
+});
--- components/RepoDetailDialog.tsx
+++ components/RepoDetailDialog.tsx
@@ -1,17 +1,26 @@
 "use client";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { isRepoDialogOpen } from "./state/isRepoDialogOpen";
-import { clickedRepo } from "./state/clickedRepo";
+import { isRepoDialogOpen } from "../app/state/isRepoDialogOpen";
+import { clickedRepo } from "../app/state/clickedRepo";
 
-export const RepoDetailModal = () => {
+export const RepoDetailDialog = () => {
   const [isOpen, setIsOpen] = useRecoilState(isRepoDialogOpen);
   const repo = useRecoilValue(clickedRepo);
+
+  const handleExceptClick = (e: React.MouseEvent<HTMLElement>) => {
+    if (e.target === e.currentTarget) {
+      setIsOpen(false);
+    } else return;
+  };
 
   return (
     <>
       {isOpen && (
-        <div className="bg-gray-400/50 fixed inset-0 w-screen h-screen">
-          <div className="bg-white fixed inset-0 m-auto w-11/12 max-w-screen-sm h-5/6">
+        <div
+          className="bg-gray-400/50 fixed inset-0 w-screen h-screen"
+          onClick={handleExceptClick}
+        >
+          <div className="bg-white fixed inset-0 m-auto w-11/12 max-w-screen-sm h-5/6 overflow-scroll">
             <p>This is a modal window.</p>
             <div className="flex items-center">
               <div>
