3cc529e02cb40b2ce2f1c9f3712584aaa113c887
Kazuki Takane <highjumptakane@gmail.com>
2023-07-31 11:56:54 +0900

    ページネーション追加

--- app/Pagination.tsx
+++ app/Pagination.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
 
-export const Pagination = () => {
+type Props = {
+  currentPage: number;
+  numOfPages: number;
+  setNumOfCurrentPage: any;
+};
+
+export const Pagination = ({
+  currentPage,
+  numOfPages,
+  setNumOfCurrentPage,
+}: Props) => {
+  const handleClick = (i: number) => {
+    setNumOfCurrentPage(i);
+  };
+  console.log(currentPage);
+  console.log(numOfPages);
+
   return (
-    <div>Pagination</div>
-  )
-}
+    <ul className="flex">
+      {[...Array(numOfPages)].map((_, i) =>
+        currentPage === i ? (
+          <li className="ml-8 bg-slate-300 cursor-pointer" key={i} onClick={() => handleClick(i)}>
+            {i + 1}
+          </li>
+        ) : (
+          <li className="ml-8 cursor-pointer" key={i} onClick={() => handleClick(i)}>
+            {i + 1}
+          </li>
+        )
+      )}
+    </ul>
+  );
+};
