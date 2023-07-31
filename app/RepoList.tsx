f12bca4efc2a6112e9afb0ac54f3fc484a751342
Kazuki Takane <highjumptakane@gmail.com>
2023-07-31 11:56:54 +0900

    ページネーション追加

--- app/RepoList.tsx
+++ app/RepoList.tsx
@@ -1,17 +1,37 @@
 "use client";
 import React from "react";
 import { RepoListItem } from "./RepoListItem";
-import { useRecoilValue } from "recoil";
-import { searchedRepos } from "./state/searchedRepos";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { numOfPagesInRepos, searchedRepos } from "./state/searchedRepos";
+import { Pagination } from "./Pagination";
+import { currentPage } from "./state/currentPage";
 
 export const RepoList = () => {
   const repos = useRecoilValue<Array<any>>(searchedRepos);
+  const numOfPages = useRecoilValue<number>(numOfPagesInRepos);
   console.log(repos);
+  const [numOfCurrentPage, setNumOfCurrentPage] = useRecoilState(currentPage);
+  console.log(currentPage);
+  const displayedRepos = repos.slice().splice(numOfCurrentPage * 10, 10);
+
   return (
-    <ul>
-      {repos.map((repo) => (
-        <RepoListItem key={repo.id} repo={repo} />
-      ))}
-    </ul>
+    <div>
+      {repos.length ? (
+        <div>
+          <ul>
+            {displayedRepos.map((repo) => (
+              <RepoListItem key={repo.id} repo={repo} />
+            ))}
+          </ul>
+          <Pagination
+            currentPage={numOfCurrentPage}
+            numOfPages={numOfPages}
+            setNumOfCurrentPage={setNumOfCurrentPage}
+          />
+        </div>
+      ) : (
+        <p>見つかりませんでした</p>
+      )}
+    </div>
   );
 };
--- app/state/searchedRepos.ts
+++ app/state/searchedRepos.ts
@@ -1,6 +1,11 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const searchedRepos = atom<Array<any>>({
   key: "searchedRepos",
   default: [],
+});
+
+export const numOfPagesInRepos = selector<number>({
+  key: "numOfPagesInRepos",
+  get: ({ get }) => Math.round(get(searchedRepos).length / 10),
 });
